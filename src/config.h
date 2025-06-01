cb38af59188debfa757af9ad1a6230568ed9dd35
TheMeaner_ <katrikat2@protonmail.com>
2025-06-01 20:34:18 +0000

    Added toggles for showing time and window name.

--- src/config.h
+++ src/config.h
@@ -1,17 +1,20 @@
 #define BOTTOM_BAR		False
 #define BAR_HEIGHT		25
-#define BAR_VERT_PAD	15
-#define BAR_HORI_PAD	400
+#define BAR_VERT_PAD	15 
+#define BAR_HORI_PAD	400 
 #define BAR_BORDER		True
 #define BAR_BORDER_W	2
 
-#define BAR_COLOR_BG		"#56002e"
-#define BAR_COLOR_FG		"#ffffff"
+#define BAR_COLOR_BG		"#560002" 
+#define BAR_COLOR_FG		"#ffffff" 
 #define BAR_COLOR_BORDER	"#005577"
 #define BAR_FONT			"fixed"
-#define BAR_TEXT_PAD		10
+#define BAR_TEXT_PAD		10 
 
 #define BAR_WS_HIGHLIGHT_LEFT	"["
 #define BAR_WS_HIGHLIGHT_RIGHT	"]"
 #define BAR_WS_PADDING			10
 #define BAR_WS_SPACING			10
+
+#define SHOW_TIME		True
+#define SHOW_WINDOW_NAME		True
